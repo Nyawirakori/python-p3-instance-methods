cb2ca2e6cb83c9065d4c4b9c08f13883ad1301b5
Joan Nyawira Kori <korijoan171@gmail.com>
2025-05-10 19:52:19 +0300

    Classes and Instances

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,16 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def bark(self): #self allows methods to access the other methods and attributes
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
+snoopy.sit()
